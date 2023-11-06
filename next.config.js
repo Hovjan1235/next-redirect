297f9c41b2afab298349914a3a7f433fe7736baf
unknown <hovjan@gmail.com>
2023-11-06 19:53:40 +0400

    ...

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        if(process.env.REDIRECT === 'true') {
+          return [
+            {
+              source: '/:path*',
+              has: [
+                {
+                  type: 'header',
+                  key: 'referer',
+                  value: '(.*facebook.*)'
+                },
+                {
+                  type: 'header',
+                  key: 'User-Agent',
+                  value: '(^((?!Facebot|facebookexternalhit|facebookcatalog|FacebookExternalHit).)*$)'
+                }
+              ],
+              permanent: true,
+              basePath: false,
+              destination: `${process.env.GRAPHQL_ENDPOINT}/:path*`
+            },
+          ]
+        } else {
+          return []
+        }
+      },
+    };
 
 module.exports = nextConfig
